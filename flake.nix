aabc78377042eb3db59e5782fb3318a719102b8f
Jens de Waard <jcdewaard@protonmail.com>
2024-04-23 13:53:48 +0200

    nix build works

--- flake.nix
+++ flake.nix
@@ -9,26 +9,31 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      jdk8 = pkgs.zulu8;
+      mavenJdk8 = pkgs.maven.override {
+        jdk = jdk8;
+      };
       buildInputs = [
-        pkgs.zulu8
-        pkgs.maven
+        jdk8
+        mavenJdk8
       ];
     in 
   {
 
-    packages.x86_64-linux.nexus-repository-ansiblegalaxy = pkgs.maven.buildMavenPackage rec {
-        inherit buildInputs;
+    packages.x86_64-linux.nexus-repository-ansiblegalaxy = mavenJdk8.buildMavenPackage rec {
         pname = "nexus-repository-ansiblegalaxy";
         version = "0.3.0";
 
-        propagatedBuildInputs = [ ];
-
         src = self;
 
-        mvnHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+        mvnHash = "sha256-XynT6FBTj865t/mIjZMVtcsGiSmLCd1ZpTeaMpDARaQ=";
 
+        mvnParameters = "-PbuildKar";
+        
         installPhase = ''
-              mkdir -p $out/bin $out/share/nexus-repository-ansiblegalaxy
+          mkdir -p $out/share/
+          cp nexus-repository-ansiblegalaxy/target/*.jar $out/share/
+          cp nexus-repository-ansiblegalaxy/target/*.kar $out/share/
         '';
     };
 
